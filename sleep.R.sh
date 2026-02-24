f8bf548fc726d27bc3c241a61d69859f36fdc692
Graham <grahammb_315@hotmail.co.uk>
2026-02-24 10:06:52 +0000

    change email address

--- sleep.R.sh
+++ sleep.R.sh
@@ -3,6 +3,6 @@
 #SBATCH -J sleep1-job
 #SBATCH -c 1
 #SBATCH -o sleep-1-job.out
-#SBATCH --mail-user=dan.grose@lancaster.ac.uk
+#SBATCH --mail-user=<USERNAME>@lancaster.ac.uk
 
 srun Rscript sleep.R 120
